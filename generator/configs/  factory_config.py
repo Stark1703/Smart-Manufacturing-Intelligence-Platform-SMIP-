641ca630357902d6a4f7b79184f1fe5980298dd3
Sumanth Vempalle <118354390+Stark1703@users.noreply.github.com>
2026-08-02 21:53:06 +0200

    Update   factory_config.py

--- generator/configs/  factory_config.py
+++ generator/configs/  factory_config.py
@@ -1,1 +1,22 @@
 
+"""
+factory_config.py
+
+Factory configuration used to simulate a GIS manufacturing shop floor.
+"""
+
+FACTORY = {
+
+    "factory_name": "VoltGrid Manufacturing",
+
+    "country": "Czech Republic",
+
+    "city": "Brno",
+
+    "production_lines": 6,
+
+    "working_days_per_week": 5,
+
+    "shifts_per_day": 3
+
+}
