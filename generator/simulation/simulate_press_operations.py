371a4a08d5f237ea83c1d696513787a484064a91
Sumanth Vempalle <118354390+Stark1703@users.noreply.github.com>
2026-08-03 23:03:07 +0200

    Fix import statement for Factory Digital Twin

--- generator/simulation/simulate_press_operations.py
+++ generator/simulation/simulate_press_operations.py
@@ -20,7 +20,7 @@ from datetime import timedelta
 
 import pandas as pd
 
-from generator.configs.Factory_Digital_Twin import (
+from generator.configs.factory_digital_twin import (
     PressOperation,
     QualityResult,
 )
