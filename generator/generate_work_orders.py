0457e123563ee354f015ea8cdb075f19cd87dcab
Sumanth Vempalle <118354390+Stark1703@users.noreply.github.com>
2026-08-03 16:38:47 +0200

    Add main function for work order simulation

--- generator/generate_work_orders.py
+++ generator/generate_work_orders.py
@@ -1,1 +1,16 @@
 
+def main():
+
+    logger.info("Starting Work Order Simulation")
+
+    products = load_products()
+
+    planner = ProductionPlanner(products)
+
+    work_orders = planner.generate()
+
+    validate(work_orders)
+
+    export(work_orders)
+
+    logger.info("Simulation completed")
